8ad11563232cba1d8b657edc05d27e0b159a94b8
Giulio Zanchetta <zank@outlook.it>
2022-02-19 00:45:17 +0100

    update packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { default as EmptySection } from "./EmptySection.astro";
-export { default as FullWidthSection } from "./FullWidthSection.astro";
+export { default as Section } from "./Section.astro";
 export { default as HalfSection } from "./HalfSection.astro";
-export { default as ImageHero } from "./ImageHero.astro";
-export { default as ImageSection } from "./ImageSection.astro";
+export { default as ImageSection } from "./composed/ImageSection.astro";
+export { default as Hero } from "./Hero.astro";
